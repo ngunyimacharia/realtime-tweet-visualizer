83e2c058c73749448dd76514261b26f519818907
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2021-06-24 06:17:25 +0200

    Pusher added

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Pusher from "pusher-js";
+
+import HelloWorld from "./components/HelloWorld.vue";
+
+const PUSHER = new Pusher(process.env.VUE_APP_PUSHER_APP_KEY, {
+  cluster: process.env.VUE_APP_PUSHER_CLUSTER,
+});
+
+const CHANNEL = "tweets";
+
+const EVENT = "new-tweet";
 
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+
+  mounted() {
+    const channel = PUSHER.subscribe(CHANNEL);
+    channel.bind(EVENT, (data) => {
+      console.log(data);
+    });
+  },
+
+  beforeDestroy() {
+    PUSHER.unsubscribe(CHANNEL);
+  },
+};
 </script>
 
 <style>
