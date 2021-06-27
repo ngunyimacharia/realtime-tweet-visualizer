795e6a1e47cbd0716e61dab35d5f0b4847478098
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2021-06-27 11:17:54 +0200

    Make hero section sticky

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,10 +1,11 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <div class="py-12 bg-gray-50 overflow-hidden">
+  <div class="p-4 sticky top-0 z-50 bg-gray-50 overflow-hidden">
     <div class="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
       <div class="relative">
         <h2
           class="
+            p-4
             text-center text-3xl
             leading-8
             font-extrabold
@@ -15,11 +16,12 @@
         >
           Developer confrence tweets visualizer
         </h2>
-        <p class="my-16 max-w-3xl mx-auto text-center text-md text-gray-500">
+        <p class="p-4 max-w-3xl mx-auto text-center text-md text-gray-500">
           Diplaying tweets in the following hash-tags:
-          <br />
-          <b
-            >#javascript, #js, #python, #java, #php, #rubyonrails, #typescript,
+        </p>
+        <p class="p-4 max-w-3xl mx-auto text-center text-md text-gray-500">
+          <b>
+            #javascript, #js, #python, #java, #php, #rubyonrails, #typescript,
             #dart, #flutter
           </b>
         </p>
