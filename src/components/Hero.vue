71ad7116c38e803c4c067de476705d609de0ed8a
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2021-06-27 18:10:29 +0200

    Cloudinary hashtag added

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -22,7 +22,7 @@
         <p class="p-4 max-w-3xl mx-auto text-center text-md text-gray-500">
           <b>
             #javascript, #js, #python, #java, #php, #rubyonrails, #typescript,
-            #dart, #flutter
+            #dart, #flutter, #cloudinary
           </b>
         </p>
       </div>
